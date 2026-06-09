b8e112972f4194e2e557d3af2eab95a35e0621e9
Bergbok <66174189+Bergbok@users.noreply.github.com>
2026-06-09 21:13:44 +0200

    chore: update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import vue from '@vitejs/plugin-vue';
 import unfonts from 'unplugin-fonts/vite';
+import buildRSS from './src/scripts/buildRSS.ts';
+import buildDoom from './src/scripts/buildDoom.ts';
+import buildOimo from './src/scripts/buildOimo.ts';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import buildGodot from './src/scripts/buildGodot.ts';
+import buildQuake from './src/scripts/buildQuake.ts';
+import buildQuake2 from './src/scripts/buildQuake2.ts';
+import buildDiablo from './src/scripts/buildDiablo.ts';
 import buildNoclip from './src/scripts/buildNoclip.ts';
+import buildCeleste from './src/scripts/buildCeleste.ts';
+import buildWipEout from './src/scripts/buildWipEout.ts';
+import buildOpenRCT2 from './src/scripts/buildOpenRCT2.ts';
 import patchSubmodules from './src/scripts/patchSubmodules.ts';
+import buildThePowderToy from './src/scripts/buildThePowderToy.ts';
+import buildAnimalCrossing from './src/scripts/buildAnimalCrossing.ts';
 import buildBassoonTracker from './src/scripts/buildBassoonTracker.ts';
+import buildSpaceCadetPinball from './src/scripts/buildSpaceCadetPinball.ts';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { Unhead as unhead } from '@unhead/vue/vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { cloudflare } from '@cloudflare/vite-plugin';
+import { cloudflareRedirect } from 'vite-plugin-cloudflare-redirect';
 
 const ᱻ = import.meta.dirname;
 
@@ -30,9 +44,25 @@ export default defineConfig(({ command }) => ({
 			}
 		}
 	},
+	define: {
+		global: 'globalThis'
+	},
 	plugins: [
+		command === 'build' && buildSpaceCadetPinball(),
 		command === 'build' && buildBassoonTracker(),
+		command === 'build' && buildAnimalCrossing(),
+		command === 'build' && buildThePowderToy(),
+		command === 'build' && buildOpenRCT2(),
+		command === 'build' && buildWipEout(),
+		command === 'build' && buildCeleste(),
+		command === 'build' && buildDiablo(),
 		command === 'build' && buildNoclip(),
+		command === 'build' && buildQuake2(),
+		command === 'build' && buildQuake(),
+		command === 'build' && buildGodot(),
+		command === 'build' && buildDoom(),
+		command === 'build' && buildOimo(),
+		command === 'build' && buildRSS(),
 		command === 'build' &&
 			createHtmlPlugin({
 				minify: {
@@ -40,6 +70,7 @@ export default defineConfig(({ command }) => ({
 				}
 			}),
 		cloudflare(),
+		cloudflareRedirect(),
 		patchSubmodules(),
 		unfonts({
 			inlineFontFace: true,
@@ -53,7 +84,7 @@ export default defineConfig(({ command }) => ({
 							category: 'monospace'
 						},
 						local: 'Departure Mono Nerd Font',
-						src: './src/assets/fonts/Departure Mono/default.woff2'
+						src: './src/assets/fonts/departure-mono.woff2'
 					},
 					{
 						name: 'Fraps',
@@ -61,6 +92,13 @@ export default defineConfig(({ command }) => ({
 							category: 'sans-serif'
 						},
 						src: './src/assets/fonts/fraps.woff2'
+					},
+					{
+						name: 'Pussyfoot',
+						fallback: {
+							category: 'sans-serif'
+						},
+						src: './src/assets/fonts/pussyfoot.woff2'
 					}
 				]
 			}
@@ -84,23 +122,18 @@ export default defineConfig(({ command }) => ({
 			'@router': resolve(ᱻ, 'src/router/router.ts'),
 			'@scripts': resolve(ᱻ, 'src/scripts'),
 			'@store': resolve(ᱻ, 'src/store'),
+			'@typings': resolve(ᱻ, 'src/types'),
 			'@views': resolve(ᱻ, 'src/views'),
 			'App.vue': resolve(ᱻ, 'src/App.vue'),
+			'gl-bench': resolve(ᱻ, 'node_modules/gl-bench/dist/gl-bench.module.js'),
 			'shaders/vue': resolve(ᱻ, 'src/lib/shaders.ts'),
 			'shaders': resolve(ᱻ, 'node_modules/shaders/dist/vue/index.js')
 		}
 	},
 	server: {
 		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'credentialless'
-		},
-		proxy: {
-			'/twitch.js': {
-				target: 'https://player.twitch.tv',
-				changeOrigin: true,
-				rewrite: (): string => '/js/embed/v1.js'
-			}
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'Cross-Origin-Opener-Policy': 'same-origin'
 		}
 	}
 }));
