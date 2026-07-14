24c109ee8faec4b5937c49e684b97a59cec3ffc3
Bergbok <66174189+Bergbok@users.noreply.github.com>
2026-07-15 00:34:21 +0200

    feat: update entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import App from 'App.vue';
+import router from '@router';
 import { createApp } from 'vue';
-import App from './App.vue';
+import { createPinia } from 'pinia';
+import { createHead } from '@unhead/vue/client';
+import { CanonicalPlugin, InferSeoMetaPlugin } from '@unhead/vue/plugins';
 
-const root = document.createElement('div');
-root.id = 'app';
-document.body.appendChild(root);
+const app = createApp(App);
+const head = createHead({
+	plugins: [
+		CanonicalPlugin({
+			canonicalHost: 'https://bergbok.computer'
+		}),
+		InferSeoMetaPlugin()
+	]
+});
+const pinia = createPinia();
 
-createApp(App).mount('#app');
+app.use(head);
+app.use(pinia);
+app.use(router);
+app.mount(document.body);
